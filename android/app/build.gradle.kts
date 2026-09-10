600bee00ced1f0e911c0172d4fb63d57175db218
nikhilesh <5258nikhilesh@gmail.com>
2026-09-10 22:59:12 +0530

    chore(release): bump version to v2.0.0 (code 35)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         applicationId = "com.torxone.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 34
-        versionName = "1.0.30"
+        versionCode = 35
+        versionName = "2.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
